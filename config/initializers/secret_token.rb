9168066b50672af8c8fad25b2b792cf131df6620
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-11-08 13:12:23 -0600

    Initial commit of a default Rails app. Soon to be twitter.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '83912098261ffc729078faea2b295f2a5251882ed1442c106d72a8ad6f3ac76ecd7c83e51bcc75c3f2d506f213a282cb9eb48d03ed98e84c8e4e6243357ef14b'
