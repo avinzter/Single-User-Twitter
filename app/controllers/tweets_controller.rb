3feee9d227903a4217a0ee787739db3077515a36
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-11-08 15:42:59 -0600

    Add error and success message

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,9 +8,15 @@ class TweetsController < ApplicationController
   	@tweet = Tweet.new(params[:tweet])
 
   	if @tweet.save
+
+  		flash[:success] = "Your tweet was successfully posted."
+
   		#If our validates, then redirect.
   		redirect_to :action => :index
   	else
+
+  		flash.now[:error] = "Sorry, all tweets must be from 1 to 140 character length"
+
   		@tweets = Tweet.order('id DESC') 
   		render :action => :index
   	end
